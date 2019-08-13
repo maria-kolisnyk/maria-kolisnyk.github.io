e0c215b7ae9a443f17e8746333acfb10da395dd1
Mariia Kolisnyk <kolisnyk.mariia@gmail.com>
2019-08-13 17:00:18 +0300

    changes

--- halifax.app.js
+++ halifax.app.js
@@ -19,8 +19,39 @@ document.addEventListener("DOMContentLoaded", function(event) {
       '        </ul>\n' +
       '    </div>';
 
-    footerBlock.innerHTML += innerSection;
+    footerBlock.innerHTML = innerSection;
   }
-
   initiateFooter();
+
+  function initiatePolicy(){
+    const passwordPolicy = document.getElementsByClassName('password-policy')[0];
+    const innerPolicy = '<p>Your password must contain at least <strong>8 characters</strong> containing <strong>uppercase</strong>, <strong>lowercase</strong>, <strong>a number</strong>, and <strong>a special character</strong>.</p>';
+
+    passwordPolicy.innerHTML = innerPolicy;
+  }
+  initiatePolicy();
+
+  const input = document.getElementById('txtNewPassword');
+
+  input.oninput = function(){
+    const password = input.value;
+
+    const passwordLength = /(?=.{8,})/.test(password);
+    const containsDigits = /[0-9]/.test(password);
+    const containsSpecial = /[@%+\\/'!#$^&?:.(){}[\]~]/.test(password);
+    const containsUpper = /[A-Z]/.test(password);
+    const containsLower = /[a-z]/.test(password);
+
+    const length = passwordLength ? '<strong class="done">8 characters</strong>' : '<strong>8 characters</strong>';
+    const digits = containsDigits ? '<strong class="done">a number</strong>' : '<strong>a number</strong>';
+    const special = containsSpecial ? '<strong class="done">a special character</strong>' : '<strong>a special character</strong>';
+    const uppercase = containsUpper ? '<strong class="done">uppercase</strong>' : '<strong>uppercase</strong>';
+    const lowercase = containsLower ? '<strong class="done">lowercase</strong>' : '<strong>lowercase</strong>';
+
+    const passwordPolicy = document.getElementsByClassName('password-policy')[0];
+
+    const innerPolicy = '<p>Your password must contain at least ' + length + ' containing ' + uppercase + ', ' + lowercase + ', ' + digits + ', and ' + special + '</p>';
+
+    passwordPolicy.innerHTML = innerPolicy;
+  }
 });
